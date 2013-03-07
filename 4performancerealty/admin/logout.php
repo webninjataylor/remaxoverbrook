486d9335e794c1beb18b5bea1947b38e911f512b
Taylor Johnson <webninjataylor@gmail.com>
2013-03-07 16:30:57 -0500

    Cleanup and Google Web Master file

--- 4performancerealty/admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php require_once('/home/users/web/b2275/d5.remaxove/etc/conn_4pr.php'); ?>
-<?php
-	//CALL SESSION AND DESTROY
-	session_save_path("/home/users/web/b2275/d5.remaxove/public_html");
-	session_start();
-	unset($_SESSION);
-	session_destroy();
-	header("Location: login.php");
-?>
--- 4performancerealty/admin/sessioncheck.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//IF NOT LOGGED IN, GO TO LOGIN
-	session_save_path("/home/users/web/b2275/d5.remaxove/public_html");
-	session_start();
-	if (!isset($_SESSION['accesslevel'])) {
-		header("Location: login.php");
-		exit();   //NEEDED TO TERMINATE SCRIPT (otherwise crawlers will delete all data)
-	}
-?>
